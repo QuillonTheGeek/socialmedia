2272da3d35ffa79e1afe2164515038fb8d664d3f
Quillon <quillonthegeek@gmail.com>
2023-02-08 04:01:57 -0800

    initaial commit

---
+++ src/pages/login/Login.jsx
@@ -0,0 +1,28 @@
+import './Login.scss'
+
+const Login = () => {
+  return (
+    <div className='login'>
+        <div className="card">
+            <div className="left">
+                <h1>Hello User!</h1>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Deserunt provident dignissimos eum, tempora et quo recusandae vero itaque atque ratione sit temporibus molestiae earum, cupiditate aspernatur iusto tempore quas rem.</p>
+                
+                <span>Do you have an account?</span>
+
+                <button>Register</button>
+            </div>
+            <div className="right">
+                <h1>Login</h1>
+                <form action="">
+                    <input type="text" placeholder='Username' />
+                    <input type="password" placeholder='Password' />
+                    <button>Login</button>
+                </form>
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default Login
---
+++ src/pages/register/Register.jsx
@@ -0,0 +1,29 @@
+import './Register.scss'
+
+const Register = () => {
+  return (
+    <div className='register'>
+        <div className="card">
+            <div className="left">
+                <h1>Quillon Social.</h1>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Deserunt provident dignissimos eum, tempora et quo recusandae vero itaque atque ratione sit temporibus molestiae earum, cupiditate aspernatur iusto tempore quas rem.</p>
+                
+                <span>Do you have an account?</span>
+
+                <button>Login</button>
+            </div>
+            <div className="right">
+                <h1>Register</h1>
+                <form action="">
+                    <input type="text" placeholder='Username' />
+                    <input type="email" placeholder='Email' />
+                    <input type="text" placeholder='Name' />
+                    <input type="password" placeholder='Password' />
+                    <button>Register</button>
+                </form>
+            </div>
+        </div>
+    </div>)
+}
+
+export default Register
